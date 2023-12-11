8c80919ea39fdfc8bc07ce3bda595bc4488c7e52
Huy Cao <caominhgiahuy@gmail.com>
2023-12-11 15:16:45 +0100

    Section 9 JWT

--- frontend/src/app/interceptors/app.request.interceptor.ts
+++ frontend/src/app/interceptors/app.request.interceptor.ts
@@ -12,24 +12,27 @@ export class XhrInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let httpHeaders = new HttpHeaders();
-    if(sessionStorage.getItem('userdetails')){
+    if (sessionStorage.getItem('userdetails')){
       this.user = JSON.parse(sessionStorage.getItem('userdetails')!);
     }
-    if(this.user && this.user.password && this.user.email){
+    if (this.user && this.user.password && this.user.email) {
       httpHeaders = httpHeaders.append('Authorization', 'Basic ' + window.btoa(this.user.email + ':' + this.user.password));
+    } else {
+      let authorization = sessionStorage.getItem('Authorization');
+      if(authorization){
+        httpHeaders = httpHeaders.append('Authorization', authorization);
+      }
     }
-
     let xsrf = sessionStorage.getItem('XSRF-TOKEN');
-
     if (xsrf) {
       httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);
     }
-
     httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
     const xhr = req.clone({
       headers: httpHeaders
     });
-  return next.handle(xhr).pipe(tap(
+
+    return next.handle(xhr).pipe(tap(
       (err: any) => {
         if (err instanceof HttpErrorResponse) {
           if (err.status !== 401) {
@@ -37,6 +40,6 @@ export class XhrInterceptor implements HttpInterceptor {
           }
           this.router.navigate(['dashboard']);
         }
-      }));
+    }));
   }
 }
--- src/main/java/com/spring/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/spring/springsecurity/config/ProjectSecurityConfig.java
@@ -1,9 +1,6 @@
 package com.spring.springsecurity.config;
 
-import com.spring.springsecurity.filter.AuthoritiesLoggingAfterFilter;
-import com.spring.springsecurity.filter.AuthoritiesLoggingAtFilter;
-import com.spring.springsecurity.filter.CsrfCookieFilter;
-import com.spring.springsecurity.filter.RequestValidationBeforeFilter;
+import com.spring.springsecurity.filter.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +17,8 @@ import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -29,9 +28,7 @@ public class ProjectSecurityConfig {
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
     requestHandler.setCsrfRequestAttributeName("_csrf");
-
-    http.securityContext(context -> context.requireExplicitSave(false))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
           @Override
           public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -40,22 +37,25 @@ public class ProjectSecurityConfig {
             config.setAllowedMethods(Collections.singletonList("*"));
             config.setAllowCredentials(true);
             config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setExposedHeaders(Arrays.asList("Authorization"));
             config.setMaxAge(3600L);
             return config;
           }
-        })).csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/register")
+        })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact","/register")
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-            .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
-            .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
-            .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
-        .authorizeHttpRequests(requests -> requests
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+        .addFilterAt(new AuthoritiesLoggingAtFilter(),BasicAuthenticationFilter.class)
+        .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+        .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+        .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+        .authorizeHttpRequests((requests)->requests
             .requestMatchers("/myAccount").hasRole("USER")
-            .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+            .requestMatchers("/myBalance").hasAnyRole("USER","ADMIN")
             .requestMatchers("/myLoans").hasRole("USER")
             .requestMatchers("/myCards").hasRole("USER")
             .requestMatchers("/user").authenticated()
-            .requestMatchers("/notices", "/contact", "/register").permitAll())
+            .requestMatchers("/notices","/contact","/register").permitAll())
         .formLogin(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults());
     return http.build();
